6117b948dea177803545e355edc73951fb8c5456
Shriram <shriram@Shrirams-Macbook-Pro.local>
2024-10-16 22:40:28 +0530

    lesgooo

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -1,8 +1,23 @@
 #ifndef HEADER_SERVER
 #define HEADER_SERVER
 
+#include <stdint.h>
+#include <stdexcept>
 #include <enet/enet.h>
 
+class Server{
+    public:
+        Server(uint32_t port);
+        ~Server();
+	    void send(const void* buffer, size_t size, ENetPeer* peer, _ENetPacketFlag flag);
+	    void disconnect(ENetPeer* peer);
+
+        ENetHost* serverHost;
+	    ENetEvent event;
+	    ENetAddress address;
+
+private:
+};
 
 
 #endif
