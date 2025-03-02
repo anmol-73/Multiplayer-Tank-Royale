e74ac50a8661635df9ac9226aff4451ba35e1dd8
Shriram <ep23b019@smail.iitm.ac.in>
2025-03-02 13:00:38 +0530

    lobby window cleaned

--- src/client/pages/lobby/lobby_ui.cpp
+++ src/client/pages/lobby/lobby_ui.cpp
@@ -1,10 +1,11 @@
 #include "lobby_ui.hpp"
+const float y_position = 0.20f;
 
 Pages::LobbyUI::LobbyUI()
 {
     using Mode = UI::DrawParameters::SizeMode;
     
-    const size_t max_room_count = 16;
+    const size_t max_room_count = 14;
     room_selects.assign(max_room_count, nullptr);
     
     auto *bg = new UI::Elements::ImageView(
@@ -29,12 +30,12 @@ Pages::LobbyUI::LobbyUI()
                             {
                                 .position = {
                                     .value = {
-                                        i < max_room_count/2 ? 0.05f : 0.35f, 0.25f + (i % (max_room_count/2)) * 0.1f
+                                        i < max_room_count/2 ? 0.05f : 0.45f, 0.25f + (i % (max_room_count/2)) * 0.1f
                                     },
                                     .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
                                 },
                                 .size = {
-                                    .value = {0.23f, 0.06f},
+                                    .value = {0.34f, 0.06f},
                                     .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
                                 },
                                 .origin = {0},
@@ -59,13 +60,66 @@ Pages::LobbyUI::LobbyUI()
             );
         }
     }
+    auto *ip_name_text = new UI::Elements::Text(
+        [](auto _) -> std::pair<UI::DrawParameters::Box, UI::DrawParameters::Text> {
+            return {
+                { // Box (for alignment)
+                    .position = {
+                        .value = {0.10f, 0.14f},  // Position of "Room Name"
+                        .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
+                    }
+                },
+                { // Text properties
+                    .content = "Server Address : 127.0.0.1",
+                    .font_size = Global::rem,
+                    .font_color = {0x45, 0x41, 0x39, 0xc0},
+                    .position = {
+                        .value = {0.0f, 0.5f},  // Centered within the box
+                        .mode = {Mode::SELF_W, Mode::SELF_H}
+                    },
+                    .origin = {
+                        .value = {0, 0.5},  // Align vertically
+                        .mode = {Mode::SELF_W, Mode::SELF_H}
+                    }
+                }
+            };
+        }
+    );
+    bg->register_element(ip_name_text);
+
+    auto *room_name_text = new UI::Elements::Text(
+        [](auto _) -> std::pair<UI::DrawParameters::Box, UI::DrawParameters::Text> {
+            return {
+                { // Box (for alignment)
+                    .position = {
+                        .value = {0.10f, y_position},  // Position of "Room Name"
+                        .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
+                    }
+                },
+                { // Text properties
+                    .content = "Room Name:",
+                    .font_size = Global::rem,
+                    .font_color = {0x45, 0x41, 0x39, 0xc0},
+                    .position = {
+                        .value = {0.0f, 0.5f},  // Centered within the box
+                        .mode = {Mode::SELF_W, Mode::SELF_H}
+                    },
+                    .origin = {
+                        .value = {0, 0.5},  // Align vertically
+                        .mode = {Mode::SELF_W, Mode::SELF_H}
+                    }
+                }
+            };
+        }
+    );
+    bg->register_element(room_name_text);
 
 
     { // Room creation
         bg->register_element(
             create_room_name_input = Components::create_sl_input(
-                "Room Name:", "...", {
-                    .value = {0.5f, 0.5f},
+                "", "...", {
+                    .value = {0.43f, 0.20f},
                     .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
                 }, {
                     .value = {0.55f, 0.05f},
@@ -79,7 +133,7 @@ Pages::LobbyUI::LobbyUI()
             create_room_button = Components::create_span_button(
                 "Create Room",
                 {
-                    .value = {0.5, 0.7},
+                    .value = {0.78f, 0.20f},
                     .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
                 }
             )
