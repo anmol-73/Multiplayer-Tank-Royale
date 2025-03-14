8636a6effbf152675cc8e66a35af0266e2a1b566
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2025-03-14 17:46:55 +0530

    game over ui done

--- src/client/pages/game_over/game_over_ui.cpp
+++ src/client/pages/game_over/game_over_ui.cpp
@@ -17,11 +17,51 @@ Pages::GameOverUI::GameOverUI()
     );
     register_element(bg);
 
+    { // Player list
+        for (size_t i = 0; i < 12; ++i){ // The names of every player in the lobby
+            bg->register_element(
+                new UI::Elements::Text(
+                    [i, this](UI::Elements::Text* text){
+                        return std::pair<UI::DrawParameters::Box, UI::DrawParameters::Text>{
+                            {
+                                .position = {
+                                    .value = {
+                                        i < 12/2 ? 0.05f : 0.35f, 0.15f + (i % (12/2)) * 0.1f
+                                    },
+                                    .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
+                                },
+                                .size = {
+                                    .value = {0.26f, 0.06f},
+                                    .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
+                                },
+                                .origin = {0},
+                                .fill = i < scores.size() ? Color{0xb2, 0xad, 0x99, 0xc0} : Color{0x49, 0x47, 0x3f, 0x00}
+                            },
+                            {
+                                .content = i < scores.size() ? scores[i].first + ": " + std::to_string(scores[i].second) : "",
+                                .font_size = Global::rem,
+                                .font_color = {0x45, 0x41, 0x39, 0xc0},
+                                .position = {
+                                    .value={0.05, 0.5},
+                                    .mode = {Mode::SELF_W, Mode::SELF_H}
+                                },
+                                .origin = {
+                                    .value = {0, 0.5},
+                                    .mode = {Mode::SELF_W, Mode::SELF_H}
+                                }
+                            }
+                        };
+                    }
+                )
+            );
+        }
+    }
+
     bg->register_element(
         go_to_lobby = Components::create_span_button(
-            "Connect",
+            "Return",
             {
-                .value = {0.5, 0.7},
+                .value = {0.5, 0.8},
                 .mode = {Mode::SCREEN_W, Mode::SCREEN_H}
             }
         )
